bf902a0aa20a25e4dd89fd9ea204edd947b6818d
rafaelanchieta <ranchieta@outlook.com>
2021-09-17 11:09:08 -0300

    initial version

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import numpy
-import pandas
-from enelvo import normaliser
+import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import classification_report
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from xgboost import XGBClassifier
 
 import util
 from generate_graph import GenerateGraph
@@ -18,25 +12,19 @@ if __name__ == '__main__':
     train_corpus = 'prep/train.csv'
     test_data = util.read_corpus(test_corpus)
     train_data = util.read_corpus(train_corpus)
-    # util.update_csv(dev_data)
-    # exit()
-    # norm = normaliser.Normaliser()
+
     graph, node_sentence = GenerateGraph(train_data, test_data).generate_graph()
+
     train_labels = util.get_labels(train_data)
     dev_labels = util.get_labels(test_data)
-    reg = Regularization().regularizer(graph, node_sentence, train_labels, '', pre_annotated=0.4, method='gnetmine')
-    train = pandas.read_csv('features_6720_pre_annotated_train.csv')
-    dev = pandas.read_csv('features_6720_pre_annotated_test.csv')
+    reg = Regularization().regularizer(graph, node_sentence, train_labels, 'features', pre_annotated=0.3, method='gfhf')
+    train = pd.read_csv('features/train.csv')
+    dev = pd.read_csv('features/test.csv')
 
-    # clf = MLPClassifier(solver='adam', random_state=42, max_iter=1000, verbose=True)
-    clf = SVC(verbose=True)
-    # clf = GaussianNB()
-    # clf = DecisionTreeClassifier()
-    # clf = GradientBoostingClassifier()
-    # clf = XGBClassifier(n_estimators=500)
+    clf = GradientBoostingClassifier(n_estimators=500, max_depth=5)
+
     clf.fit(train[['toxic', 'nontoxic']], numpy.ravel(train[['label']]))
-    print('Training: ', clf.score(dev[['toxic', 'nontoxic']], dev_labels))
+    # print('Training: ', clf.score(dev[['toxic', 'nontoxic']], dev_labels))
     y_pred = clf.predict(dev[['toxic', 'nontoxic']])
 
     print(classification_report(dev_labels, y_pred))
-    # print(util.normalizer(util.preprocess(test_data['text'][13])))
