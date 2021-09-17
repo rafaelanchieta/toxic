d0723642ae60aa7acc68d308712fd0011d5c7e49
rafaelanchieta <ranchieta@outlook.com>
2021-09-17 11:09:35 -0300

    regularization

--- regularization.py
+++ regularization.py
@@ -1,5 +1,5 @@
 from random import shuffle
-
+import os
 import pandas as pd
 
 from GNetMine import GNetMine
@@ -19,17 +19,21 @@ class Regularization:
         shuffle(cods)
 
         annotated = cods[0:int(total_samples * pre_annotated)]
+        print(len(annotated))
+        print(total_samples)
         train = 'train_%d'
         # test = 'test_%d'
 
         key_annotated = []
+        cont = 0
         for i in range(total_samples):
             if i in annotated:
+                cont += 1
                 graph.nodes[sentence_nodes[train % i]]['label'] = train_labels[i]
                 key_annotated.append(train % i)
         with open('annotated.txt', 'w') as f:
             for i in annotated:
-                f.write('%d\n' %i)
+                f.write('%d\n' % i)
 
         labels = ['toxic', 'nontoxic']
         columns = ['id', labels[0], labels[1], 'label']
@@ -67,10 +71,10 @@ class Regularization:
                 elif t == 'dev':
                     rows_test.append([id_node, f[dict_nodes[id_node]][0], f[dict_nodes[id_node]][1]])
 
-        file_train = 'features_%s_pre_annotated_train.csv' % len(annotated)
-        file_test = 'features_%s_pre_annotated_test.csv' % len(annotated)
+        file_train = 'train.csv'
+        file_test = 'test.csv'
 
         df_train = pd.DataFrame(rows_train, columns=columns)
         df_test = pd.DataFrame(rows_test, columns=columns[:3])
-        df_train.to_csv(path_out + file_train, index=False)
-        df_test.to_csv(path_out + file_test, index=False)
+        df_train.to_csv(os.path.join(path_out, file_train), index=False)
+        df_test.to_csv(os.path.join(path_out, file_test), index=False)
